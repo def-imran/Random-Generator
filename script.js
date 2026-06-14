7d64e0e227c36b6808faa8accbcfe9b179c0e22e
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-06-14 13:29:21 +0200

    update quotes in script.js and adjust generateQuote function for improved functionality

--- script.js
+++ script.js
@@ -5,27 +5,31 @@ const quotes = [
     },
 
     {
-        "quote": "test1",
-        "name": "test1name"
+        "quote": "test2",
+        "name": "test2name"
     },
 
     {
-        "quote": "test1",
-        "name": "test1name"
+        "quote": "test3",
+        "name": "test3name"
     },
 
     {
-        "quote": "test1",
-        "name": "test1name"
+        "quote": "test4",
+        "name": "test4name"
     },
 
     {
-        "quote": "test1",
-        "name": "test1name"
+        "quote": "test5",
+        "name": "test5name"
     }
 ]
 
+const quoteButton = document.getElementById("generate-button")
+quoteButton.addEventListener("click", (generateQuote))
+
 function generateQuote() {
-    const quoteButton = document.getElementById("generate-button")
+    const randomQuote = quotes[Math.floor(quotes.length * Math.random())];
+    console.log(randomQuote.quote, randomQuote.name)
 
 }
