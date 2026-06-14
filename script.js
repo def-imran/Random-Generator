a5996c7e35bed2a50d977e0dbb68298232bc095e
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-06-14 13:23:59 +0200

    update quotes array in script.js and change quote visualizer display style in style.css

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+const quotes = [
+    {
+        "quote": "test1",
+        "name": "test1name"
+    },
 
+    {
+        "quote": "test1",
+        "name": "test1name"
+    },
 
-function generateQuote(){
+    {
+        "quote": "test1",
+        "name": "test1name"
+    },
+
+    {
+        "quote": "test1",
+        "name": "test1name"
+    },
+
+    {
+        "quote": "test1",
+        "name": "test1name"
+    }
+]
+
+function generateQuote() {
     const quoteButton = document.getElementById("generate-button")
 
 }
