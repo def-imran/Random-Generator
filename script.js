23ae9344446dd4c887e6b081b710ec0ccd5cabed
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-06-15 15:52:14 +0200

    update quotes in script.js to include new sayings attributed to Prophet Muhammed for enhanced content diversity

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 const quotes = [
     {
-        "quote": "He who alleviates the suffering of a brother out of the sufferings of the world, Allah would alleviate his suffering from the sufferings of the Day of Resurrection",
-        "name": "prophet Muhammed peace be upon him."
+        "quote": "Anybody who believes in Allah and the Last Day should not harm his neighbor",
+        "name": "Prophet Muhammed peace be upon him"
     },
-
     {
-        "quote": "test2",
-        "name": "test2name"
+        "quote": "The most complete of the believers in faith, is the one with the best character among them. And the best of you are those who are best to your women.",
+        "name": "Prophet Muhammed peace be upon him"
     },
-
     {
-        "quote": "test3",
-        "name": "test3name"
+        "quote": "Your smiling in the face of your brother is charity",
+        "name": "Prophet Muhammed peace be upon him"
     },
-
     {
-        "quote": "test4",
-        "name": "test4name"
+        "quote": "Whoever has three daughters, or three sisters, or two daughters, or two sisters and he keeps good company with them and fears Allah regarding them, then Paradise is for him.",
+        "name": "Prophet Muhammed peace be upon him"
     },
-
     {
-        "quote": "test5",
-        "name": "test5name"
+        "quote": "Do not hate one another, and do not be jealous of one another, and do not desert each other",
+        "name": "Prophet Muhammed peace be upon him"
+    },
+    {
+        "quote": "There is none amongst the Muslims who plants a tree or sows seeds, and then a bird, or a person or an animal eats from it, but is regarded as a charitable gift for him.",
+        "name": "Prophet Muhammed peace be upon him"
+    },
+    {
+        "quote": "And beware of going to extremes in religious matters, for those who came before you were destroyed because of going to extremes in religious matters.",
+        "name": "Prophet Muhammed peace be upon him"
+    },
+    {
+        "quote": "He is not one of us who does not have mercy on our young and does not respect our elders.",
+        "name": "Prophet Muhammed peace be upon him"
+    },
+    {
+        "quote": "Truthfulness leads to righteousness, and righteousness leads to Paradise. And a man keeps on telling the truth until he becomes a truthful person.",
+        "name": "Prophet Muhammed peace be upon him"
+    },
+    {
+        "quote": "The one who looks after a widow or a poor person is like a Mujahid (warrior) who fights for Allah's Cause, or like him who performs prayers all the night and fasts all the day.",
+        "name": "Prophet Muhammed peace be upon him"
     }
 ]
 
